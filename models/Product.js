3ed754d04cab346b0a1fe3f4b19e2424a184f7d0
Austin Lewandowski <austin109lew@icloud.com>
2024-03-03 09:56:32 -0500

    finished up models

--- models/Product.js
+++ models/Product.js
@@ -10,9 +10,47 @@ class Product extends Model {}
 Product.init(
   {
     id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
 
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    price: {
+      type: DataTypes.DECIMAL(10, 2), // Adjust precision and scale as needed
+      allowNull: false,
+      validate: {
+        isDecimal: {
+          msg: "Price must be a decimal value" // Custom error message (optional)
+        }
+      }
+    },
+
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: {
+          msg: "Stock must be a numeric value" // Custom error message (optional)
+        }
+      }
+    },
+
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Category', // Name of the model to reference
+        key: 'id'         // Key in the referenced model that the foreign key refers to
+      }
     }
-  },
+    
+  },  // This curly brace was moved to close the first argument
   {
     sequelize,
     timestamps: false,
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,29 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Product', // Name of the model to reference
+        key: 'id'         // Key in the referenced model that the foreign key refers to
+      }
+    },
+
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Tag', // Name of the model to reference
+        key: 'id'     // Key in the referenced model that the foreign key refers to
+      }
+    }
+    
   },
   {
     sequelize,
